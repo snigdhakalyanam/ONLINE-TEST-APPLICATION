1ca0480223b563873fb2ff4506d048a56a114919
snigdhakalyanam <snigdhakalyanam92@gmail.com>
2022-07-10 11:32:20 +0530

    initial

---
+++ js/questions.js
@@ -0,0 +1,118 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stands for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+    {
+    numb: 6,
+    question:"Which of the following JavaScript cannot do?",
+    answer: "All of the Above",
+    options: [
+      "JavaScript can react to events",
+      "JavaScript can manipulate HTML elements",
+      "JavaScript can be use to validate data",
+      "All of the Above",
+    ]
+  },
+
+  {
+    numb: 7,
+    question:"Which keyword is used to declare variables in javascript.",
+    answer: "Var",
+    options: [
+      "Var",
+      "Dim",
+      "String",
+      "None of the Above",
+    ]
+  },
+
+  {
+    numb: 8,
+    question:"In JavaScript the x===y statement implies that:",
+    answer: "Both are equal in the value and data type.",
+    options: [
+      "Both x and y are equal in value, type and reference address as well.",
+      "Both are x and y are equal in value only.",
+      "Both are equal in the value and data type.",
+      "Both are not same at all.",   
+    ]
+  },
+
+  {
+    numb: 9,
+    question:"Whats so great about XML?",
+    answer: "Both",
+    options: [
+      "Easy data exchange",
+      "High speed on network ",
+      "Both",
+      "None",   
+    ]
+  },
+
+  {
+    numb: 10,
+    question:"In JavaScript, which one of the following is not considered as an error?",
+    answer: "Division by zero",
+    options: [
+      "Syntax error",
+      "Missing of semicolons",
+      "Division by zero",
+      "Missing of Bracket",   
+    ]
+  },
+];
